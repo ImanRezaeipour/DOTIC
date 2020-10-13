144aa6f6b310df0a9fe324a970b1ea3c38158ba3
iman.rezaeipour <iman.rezaeipour@gmail.com>
2020-10-13 22:10:10 +0330

    init

---
+++ aspnet-core/src/PTC.DOTIC.Application.Client/ApiClient/IApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Web.Models.AbpUserConfiguration;
+using JetBrains.Annotations;
+using PTC.DOTIC.Sessions.Dto;
+
+namespace PTC.DOTIC.ApiClient
+{
+    public interface IApplicationContext
+    {
+        [CanBeNull]
+        TenantInformation CurrentTenant { get; }
+
+        AbpUserConfigurationDto Configuration { get; set; }
+
+        GetCurrentLoginInformationsOutput LoginInfo { get; set; }
+
+        void SetAsHost();
+
+        void SetAsTenant(string tenancyName, int tenantId);
+    }
+}
---
+++ aspnet-core/src/PTC.DOTIC.EntityFrameworkCore/EntityFrameworkCore/DOTICEntityFrameworkCoreModule.cs
@@ -0,0 +1,55 @@
+﻿using Abp.EntityFrameworkCore.Configuration;
+using Abp.IdentityServer4;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Zero.EntityFrameworkCore;
+using PTC.DOTIC.Configuration;
+using PTC.DOTIC.Migrations.Seed;
+
+namespace PTC.DOTIC.EntityFrameworkCore
+{
+    [DependsOn(
+        typeof(AbpZeroCoreEntityFrameworkCoreModule),
+        typeof(DOTICCoreModule),
+        typeof(AbpZeroCoreIdentityServerEntityFrameworkCoreModule)
+        )]
+    public class DOTICEntityFrameworkCoreModule : AbpModule
+    {
+        /* Used it tests to skip dbcontext registration, in order to use in-memory database of EF Core */
+        public bool SkipDbContextRegistration { get; set; }
+
+        public bool SkipDbSeed { get; set; }
+
+        public override void PreInitialize()
+        {
+            if (!SkipDbContextRegistration)
+            {
+                Configuration.Modules.AbpEfCore().AddDbContext<DOTICDbContext>(options =>
+                {
+                    if (options.ExistingConnection != null)
+                    {
+                        DOTICDbContextConfigurer.Configure(options.DbContextOptions, options.ExistingConnection);
+                    }
+                    else
+                    {
+                        DOTICDbContextConfigurer.Configure(options.DbContextOptions, options.ConnectionString);
+                    }
+                });
+            }
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(DOTICEntityFrameworkCoreModule).GetAssembly());
+        }
+
+        public override void PostInitialize()
+        {
+            var configurationAccessor = IocManager.Resolve<IAppConfigurationAccessor>();
+            if (!SkipDbSeed && DatabaseCheckHelper.Exist(configurationAccessor.Configuration["ConnectionStrings:Default"]))
+            {
+                SeedHelper.SeedHostDb(IocManager);
+            }
+        }
+    }
+}
